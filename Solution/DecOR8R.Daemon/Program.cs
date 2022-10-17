1963daf13a92a1851d7e104ca172a38a0ba56be6
AxlER8R <axl@axler8r.io>
2022-10-18 07:05:05 +1300

    feat(listener): add listener

--- Solution/DecOR8R.Daemon/Program.cs
+++ Solution/DecOR8R.Daemon/Program.cs
@@ -1,7 +1,35 @@
-using DecOR8R.Daemon;
+using DecOR8R.Daemon.Service;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services => { services.AddHostedService<Worker>(); })
+var host_ = Host.CreateDefaultBuilder(args)
+    .UseSystemd()
+    .ConfigureAppConfiguration((_, builder) => { })
+    .ConfigureLogging((_, builder) =>
+    {
+        builder.AddSystemdConsole(options =>
+        {
+            options.UseUtcTimestamp = true;
+            options.TimestampFormat = "yyyy-MM-ddTHH:mm:ss.fff ";
+            options.IncludeScopes = true;
+        });
+        // TODO: Investigate debug logger
+        // TODO: Investigate console log formatter
+    })
+    .ConfigureServices((_, services) =>
+    {
+        services.AddHostedService<Configurator>();
+        services.AddHostedService<Listener>();
+    })
     .Build();
 
-await host.RunAsync();
+try
+{
+    await host_.RunAsync();
+
+    return 0;
+}
+catch (Exception e_)
+{
+    Console.Error.WriteLine($"Unable to start service: {e_.Message}");
+
+    return 1;
+}
---
+++ Solution/DecOR8R.Daemon/Service/Listener.cs
@@ -0,0 +1,49 @@
+using System.Net.Sockets;
+
+namespace DecOR8R.Daemon.Service;
+
+public class Listener : BackgroundService
+{
+    private readonly ILogger<Listener> _logger;
+    private readonly IConfiguration _configuration;
+    private readonly string _socketFileName;
+
+    public Listener(ILogger<Listener> logger, IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration = configuration;
+        _socketFileName = Path.Combine(Path.GetTempPath(), "decor8rd.sock");
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("Starting {This}", GetType().FullName);
+
+        _logger.LogTrace("Preparing UNIX domain socket: {SocketFileName}", _socketFileName);
+        if (File.Exists(_socketFileName)) File.Delete(_socketFileName);
+        var endpoint_ = new UnixDomainSocketEndPoint(_socketFileName);
+
+        _logger.LogInformation("Listening for incoming connections");
+        using var listener_ = new Socket(AddressFamily.Unix, SocketType.Stream, ProtocolType.Unspecified);
+        listener_.Bind(endpoint_);
+        listener_.Listen(1);
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await Task.Delay(0, stoppingToken);
+
+            _logger.LogInformation("Accepting request {Now}", DateTime.UtcNow.ToString("O"));
+            using var socket_ = await listener_.AcceptAsync(stoppingToken);
+
+            _logger.LogTrace("Processing request");
+            await using var stream_ = new NetworkStream(socket_);
+            using var reader_ = new StreamReader(stream_);
+            var request_ = await reader_.ReadLineAsync();
+            _logger.LogTrace("Request: {Request}", request_);
+            await using var writer_ = new StreamWriter(stream_);
+            await writer_.WriteLineAsync(request_);
+            await writer_.FlushAsync();
+            _logger.LogInformation("Request processed");
+        }
+    }
+}
