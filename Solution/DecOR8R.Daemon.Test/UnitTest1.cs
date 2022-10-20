eaa679f53931013ba9581b7ec8eb3a299bc0d308
AxlER8R <axl@axler8r.io>
2022-10-20 21:02:01 +1300

    style: apply style conventions

--- Solution/DecOR8R.Daemon.Test/UnitTest1.cs
+++ Solution/DecOR8R.Daemon.Test/UnitTest1.cs
@@ -3,7 +3,5 @@ namespace DecOR8R.Daemon.Test;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
-    {
-    }
+    public void Test1() { }
 }
--- Solution/DecOR8R.Daemon/Service/Configurator.cs
+++ Solution/DecOR8R.Daemon/Service/Configurator.cs
@@ -6,7 +6,19 @@ namespace DecOR8R.Daemon.Service;
 
 public class Configurator : BackgroundService
 {
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    private readonly IConfiguration _configuration;
+    private readonly ILogger<Configurator> _logger;
+
+    public Configurator(
+        ILogger<Configurator> logger,
+        IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration = configuration;
+    }
+
+    protected override async Task ExecuteAsync(
+        CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
