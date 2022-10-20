8ee14055d76c8c35f4029e1c2318e816fe1bfcd2
AxlER8R <axl@axler8r.io>
2022-10-20 21:07:40 +1300

    style: apply style conventions

--- Solution/DecOR8R.Daemon/Program.cs
+++ Solution/DecOR8R.Daemon/Program.cs
@@ -2,23 +2,56 @@ using DecOR8R.Daemon.Service;
 
 var host_ = Host.CreateDefaultBuilder(args)
     .UseSystemd()
-    .ConfigureAppConfiguration((_, builder) => { })
-    .ConfigureLogging((_, builder) =>
-    {
-        builder.AddSystemdConsole(options =>
+    .ConfigureAppConfiguration(
+        (
+            context,
+            builder) =>
         {
-            options.UseUtcTimestamp = true;
-            options.TimestampFormat = "yyyy-MM-ddTHH:mm:ss.fff ";
-            options.IncludeScopes = true;
-        });
-        // TODO: Investigate debug logger
-        // TODO: Investigate console log formatter
-    })
-    .ConfigureServices((_, services) =>
-    {
-        services.AddHostedService<Configurator>();
-        services.AddHostedService<Listener>();
-    })
+            // prepare base path
+            var configRoot_ = Environment.GetEnvironmentVariable("$XDG_CONFIG_HOME");
+            if (string.IsNullOrEmpty(configRoot_))
+                configRoot_ =
+                    Path.Combine(
+                        Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                        ".config");
+            var configPath_ = Path.Combine(configRoot_, "decor8r");
+            builder.SetBasePath(configPath_);
+
+            // add application configuration files
+            var env_ = context.HostingEnvironment;
+            builder.AddJsonFile("appsettings.json", false, true);
+            builder.AddJsonFile($"appsettings.{env_.EnvironmentName}.json", true, true);
+
+            // add user configuration files
+            foreach (var file_ in Directory.GetFiles(
+                         configPath_,
+                         "*.json",
+                         SearchOption.TopDirectoryOnly))
+                builder.AddJsonFile(file_, true, true);
+        })
+    .ConfigureLogging(
+        (
+            _,
+            builder) =>
+        {
+            builder.AddSystemdConsole(
+                options =>
+                {
+                    options.UseUtcTimestamp = true;
+                    options.TimestampFormat = "yyyy-MM-ddTHH:mm:ss.fff ";
+                    options.IncludeScopes = true;
+                });
+            // TODO: Investigate debug logger
+            // TODO: Investigate console log formatter
+        })
+    .ConfigureServices(
+        (
+            _,
+            services) =>
+        {
+            services.AddHostedService<Configurator>();
+            services.AddHostedService<Listener>();
+        })
     .Build();
 
 try
